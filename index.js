4364d3cb6e3511870c92a2a50277afa9912130a8
Jared <kuil5230@gmail.com>
2019-05-16 18:15:26 +0800

    .

--- index.js
+++ index.js
@@ -1,55 +1,19 @@
 import cheerio from "cheerio";
 import superagent from "superagent";
-require("superagent-proxy")(superagent);
+import superagentProxy from "superagent-proxy";
+superagentProxy(superagent);
 
-import { exportResults, writeFileAsync } from "./src/fs_process";
+import { exportResults, readFileAsync, writeFileAsync } from "./src/fs_process";
 import { asyncForEach, waitFor } from "./src/delay";
+import getPorxyIp from "./src/getProxyIP";
 import userAgents from "./src/userAgents";
-
-const proxy = "http://180.183.96.150:8213";
-
-//
-// http://138.197.167.88:8080
-// http://47.90.202.224:3128
-// http://142.93.145.229:3128
-// http://104.248.112.25:8080
-// http://92.222.25.82:3128
-// http://103.43.203.225:53281
-// http://1.179.148.9:36656
-// http://1.20.97.166:33587
-// http://217.30.73.152:32368
-// http://1.20.96.111:45555
-// http://112.78.152.98:52377
-// http://201.48.194.210:80
-// http://41.169.15.99:48733
-// http://81.24.88.136:41258
-// http://125.27.10.245:43800
-// http://187.163.36.54:59753
-// http://103.9.124.210:8080
-// http://185.85.116.61:8080
-// http://185.85.117.109:8080
-// http://185.85.116.71:8080
-// http://185.85.116.90:8080
-// http://185.85.116.33:8080
-// http://185.85.116.9:8080
-// http://103.65.193.195:52115
-// http://103.9.190.206:44245
-// http://103.233.123.177:8080
-// http://195.24.154.3:51749
-// http://37.221.157.98:43570
-// http://181.10.238.7:58738
-// http://109.248.249.37:8081
-// http://182.93.94.98:46317
-// http://137.74.109.229:3128
-
-let userAgent = userAgents[parseInt(Math.random() * userAgents.length)];
 
 const headerConfig = {
   "Content-Type": "text/plain",
-  "User-Agent": userAgent
+  "User-Agent": userAgents[parseInt(Math.random() * userAgents.length)]
 };
 
-const getWebSiteContent = async (url, forumCode, page, totalCode, outputPath) => {
+const getWebSiteContent = async (url, proxy, forumCode, page, totalCode, outputPath) => {
   const pageLinkList = [];
   const getWebSitePageUrl = async () => {
     try {
@@ -94,7 +58,7 @@ const getWebSiteContent = async (url, forumCode, page, totalCode, outputPath) =>
 
       await exportResults(crawlerResultList, outputPath);
     } catch (error) {
-      console.log("TCL: RequestDataAsync -> error", error);
+      //console.log("TCL: RequestDataAsync -> error", error);
     }
   };
 
@@ -115,12 +79,17 @@ const startCrawler = async (forum, startPage, totalCode) => {
   const outputPath = `./output/${forum}_${Math.floor(Math.random() * (99 - 10 + 1)) + 10}.json`;
   writeFileAsync(outputPath, []);
 
+  const proxy = await readFileAsync("./ip.json").then(data => {
+    return JSON.parse(data)[parseInt(Math.random() * JSON.parse(data).length)];
+  });
   const list = [];
   for (let i = startPage; i <= totalCode; i++) list.push(i);
 
   await asyncForEach(list, async num => {
+    console.log(proxy);
     await getWebSiteContent(
       `https://www.backpackers.com.tw/forum/forumdisplay.php?f=${forum}&order=desc&page=${num}`,
+      proxy,
       forum,
       num,
       totalCode,
@@ -130,4 +99,6 @@ const startCrawler = async (forum, startPage, totalCode) => {
   });
 };
 
-startCrawler(116, 243, 369);
+startCrawler(116, 302, 369);
+
+//getPorxyIp();
---
+++ src/getProxyIP.js
@@ -0,0 +1,58 @@
+import cheerio from "cheerio";
+import superagent from "superagent";
+import superagentProxy from "superagent-proxy";
+superagentProxy(superagent);
+
+import { writeFileAsync } from "./fs_process";
+const requestProxyIp = () => {
+  const getProxyUrl1 = async url => {
+    const IP_proxy_RequestList = [];
+    try {
+      const RequestHTML = await superagent.get(url);
+      const $ = cheerio.load(RequestHTML.text);
+
+      $("#proxylisttable > tbody > tr").map((index, value) => {
+        const IP_http =
+          $(value)
+            .children("td:nth-child(7)")
+            .text() === "no"
+            ? "http"
+            : "https";
+        const IP_url = $(value)
+          .children("td:nth-child(1)")
+          .text();
+        const IP_port = $(value)
+          .children("td:nth-child(2)")
+          .text();
+
+        IP_proxy_RequestList.push(`${IP_http}://${IP_url}:${IP_port}`);
+      });
+
+      return IP_proxy_RequestList;
+    } catch (error) {
+      console.log("TCL: getWebSitePageUrl -> error", error);
+    }
+  };
+
+  Promise.resolve()
+    .then(() => getProxyUrl1("https://free-proxy-list.net/"))
+    .then(list => {
+      const resultList = [];
+      Promise.all(
+        list.map(async itemLink => {
+          try {
+            const testIp = await superagent
+              .get("https://www.google.com.tw")
+              .proxy(itemLink)
+              .timeout(5000);
+            if (testIp.status == 200) {
+              resultList.push(itemLink);
+            }
+          } catch (error) {}
+          writeFileAsync("./ip.json", resultList);
+        })
+      );
+    });
+};
+
+export default requestProxyIp;
